b99f85e412d0e448b1bff365e799d4082da27812
cpu <shakemidinovemir@gmail.com>
2025-01-03 00:21:15 +0600

    fist

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
 import './App.css';
-function Header () {
-  return (
-      <>
-          <Title/>
-      <div>
-        <h2>I am Header</h2>
-      </div>
-      </>
-  );
-}
 function Title () {
-  return (
-      <div>
-        <h2>I am Title</h2>
-      </div>
-  );
+    return (
+        <div>
+            <h2>I am Title</h2>
+        </div>
+    )
 }
 
-function Content () {
-  return (
-      <div>
-          <Title/>
-        <h2>I am Content</h2>
-      </div>
-  );
-}
 function Footer () {
-  return (
-      <div>
-          <Title/>
-        <h2>I am Footer</h2>
-      </div>
-  )
+    return (
+        <div>
+            <Title/>
+            <h2>I am Footer</h2>
+        </div>
+    )
+}
+
+
+
+function Content () {
+    return (
+        <div>
+            <Title/>
+            <h2>I am Content</h2>
+        </div>
+    )
+}
+
+
+
+function Header () {
+    return (
+        <div>
+            <Title/>
+            <h2>I am Header</h2>
+        </div>
+    )
 }
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Content/>
-      <Footer/>
+        <Header/>
+        <Content/>
+        <Footer/>
     </div>
   );
 }
