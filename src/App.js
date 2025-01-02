467d3760795e2dc7f73b4ba4ef19b39b98eef630
cpu <shakemidinovemir@gmail.com>
2025-01-02 22:33:42 +0600

    fist

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+function Header () {
+  return (
+      <>
+          <Title/>
+      <div>
+        <h2>I am Header</h2>
+      </div>
+      </>
+  );
+}
+function Title () {
+  return (
+      <div>
+        <h2>I am Title</h2>
+      </div>
+  )
+}
+
+function Content () {
+  return (
+      <div>
+          <Title/>
+        <h2>I am Content</h2>
+      </div>
+  );
+}
+function Footer () {
+  return (
+      <div>
+          <Title/>
+        <h2>I am Footer</h2>
+      </div>
+  )
+}
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Content/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
